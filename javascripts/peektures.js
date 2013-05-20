08e8b23ccfea4f39da22c374622c87516bb61594
Ilya Vassilevsky <vassilevsky@gmail.com>
2013-05-20 21:38:30 +0400

    Рефакторинг

--- javascripts/peektures.js
+++ javascripts/peektures.js
@@ -1,8 +1,8 @@
 (function() {
-  var $, all_friends, all_photos, all_users, auth_handler, authorize, current_pid, hash_by, hashed_users, next_keys, next_photo, previous_keys, previous_photo, show_photos, ts2date, update_main_photo,
+  var $, all_friends, all_photos, all_users, auth_handler, authorize, current_photo_id, next_keys, next_photo, previous_keys, previous_photo, show_photos, ts2date, update_main_photo, users,
     __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
-  current_pid = -1;
+  current_photo_id = -1;
 
   all_friends = [];
 
@@ -10,7 +10,7 @@
 
   all_users = [];
 
-  hashed_users = [];
+  users = {};
 
   next_keys = [32, 39, 40, 74, 34];
 
@@ -38,27 +38,20 @@
     $("photos").style.display = "";
     return FB.api("/fql", {
       q: {
-        'friends': 'SELECT uid2 FROM friend WHERE uid1 = me()',
-        'users': 'SELECT uid, name, profile_url FROM user WHERE uid IN (\
-                    SELECT uid2 FROM #friends\
-                  )',
-        'photos': 'SELECT owner,\
-                         src_big, src_big_width, src_big_height,\
-                         link,\
-                         caption,\
-                         created\
-                  FROM photo WHERE aid IN (\
-                    SELECT aid FROM album WHERE owner IN (\
-                      SELECT uid2 FROM #friends\
-                    )\
-                  ) ORDER BY created DESC'
+        "friends": "SELECT uid2 FROM friend WHERE uid1 = me()",
+        "photos": "SELECT owner, src_big, src_big_width, src_big_height, link, caption, created                  FROM photo WHERE aid IN (                    SELECT aid FROM album WHERE owner IN (                      SELECT uid2 FROM #friends                    )                  ) ORDER BY created DESC",
+        "users": "SELECT uid, name, profile_url FROM standard_user_info WHERE uid IN(                    SELECT owner FROM #photos                  )"
       }
     }, function(response) {
+      var user, _i, _len;
       if (response.data && response.data.length) {
         all_friends = response.data[0].fql_result_set;
         all_photos = response.data[1].fql_result_set;
         all_users = response.data[2].fql_result_set;
-        hashed_users = hash_by(all_users, 'uid');
+        for (_i = 0, _len = all_users.length; _i < _len; _i++) {
+          user = all_users[_i];
+          users[user.uid] = user;
+        }
         $("main_photo").onclick = next_photo;
         window.onkeydown = function(event) {
           var _ref, _ref1;
@@ -76,12 +69,12 @@
   };
 
   next_photo = function() {
-    current_pid = current_pid + 1;
+    current_photo_id++;
     return update_main_photo();
   };
 
   previous_photo = function() {
-    current_pid = current_pid - 1;
+    current_photo_id--;
     return update_main_photo();
   };
 
@@ -103,27 +96,17 @@
     $('main_caption').innerHTML = caption;
     _results = [];
     for (i = _i = 1; _i <= 10; i = ++_i) {
-      _results.push($("preload_" + i).src = all_photos[current_pid + i].src_big);
+      _results.push($("preload_" + i).src = all_photos[current_photo_id + i].src_big);
     }
     return _results;
-  };
-
-  hash_by = function(array, property) {
-    var element, hsh, _i, _len;
-    hsh = {};
-    for (_i = 0, _len = array.length; _i < _len; _i++) {
-      element = array[_i];
-      hsh[element[property]] = element;
-    }
-    return hsh;
   };
 
   ts2date = function(ts) {
     var d, ds;
     d = new Date(ts * 1000);
-    ds = "" + (d.getMonth() + 1) + "/" + d.getDate();
+    ds = "" + (d.getMonth() + 1) + "/" + (d.getDate());
     if (d.getFullYear() < (new Date()).getFullYear()) {
-      ds = ds + "/" + d.getFullYear();
+      ds = "" + ds + "/" + (d.getFullYear());
     }
     return ds;
   };
